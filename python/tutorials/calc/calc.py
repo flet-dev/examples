7b8da6ee184bdf742c020ae589160f07483a2fe5
Inesa Fitsner <inesa@appveyor.com>
2022-06-11 15:23:32 -0700

    Update calc.py

--- python/tutorials/calc/calc.py
+++ python/tutorials/calc/calc.py
@@ -13,6 +13,10 @@ from flet import (
 
 class CalcApp:
     def __init__(self):
+        self.operator = "+"
+        self.operand1 = 0
+        self.operand2 = 0
+        self.new_operand = True
         self.result = Text(value="0", color=colors.WHITE, size=20)
 
         # application's root control (i.e. "view") containing all other controls
@@ -30,6 +34,8 @@ class CalcApp:
                                 bgcolor=colors.BLUE_GREY_100,
                                 color=colors.BLACK,
                                 expand=1,
+                                on_click=self.button_clicked,
+                                data="AC",
                             ),
                             ElevatedButton(
                                 text="+/-",
@@ -58,18 +64,24 @@ class CalcApp:
                                 bgcolor=colors.WHITE24,
                                 color=colors.WHITE,
                                 expand=1,
+                                on_click=self.button_clicked,
+                                data="7",
                             ),
                             ElevatedButton(
                                 text="8",
                                 bgcolor=colors.WHITE24,
                                 color=colors.WHITE,
                                 expand=1,
+                                on_click=self.button_clicked,
+                                data="8",
                             ),
                             ElevatedButton(
                                 text="9",
                                 bgcolor=colors.WHITE24,
                                 color=colors.WHITE,
                                 expand=1,
+                                on_click=self.button_clicked,
+                                data="9",
                             ),
                             ElevatedButton(
                                 text="*",
@@ -86,18 +98,24 @@ class CalcApp:
                                 bgcolor=colors.WHITE24,
                                 color=colors.WHITE,
                                 expand=1,
+                                on_click=self.button_clicked,
+                                data="4",
                             ),
                             ElevatedButton(
                                 text="5",
                                 bgcolor=colors.WHITE24,
                                 color=colors.WHITE,
                                 expand=1,
+                                on_click=self.button_clicked,
+                                data="5",
                             ),
                             ElevatedButton(
                                 text="6",
                                 bgcolor=colors.WHITE24,
                                 color=colors.WHITE,
                                 expand=1,
+                                on_click=self.button_clicked,
+                                data="6",
                             ),
                             ElevatedButton(
                                 text="-",
@@ -114,24 +132,32 @@ class CalcApp:
                                 bgcolor=colors.WHITE24,
                                 color=colors.WHITE,
                                 expand=1,
+                                on_click=self.button_clicked,
+                                data="1",
                             ),
                             ElevatedButton(
                                 text="2",
                                 bgcolor=colors.WHITE24,
                                 color=colors.WHITE,
                                 expand=1,
+                                on_click=self.button_clicked,
+                                data="2",
                             ),
                             ElevatedButton(
                                 text="3",
                                 bgcolor=colors.WHITE24,
                                 color=colors.WHITE,
                                 expand=1,
+                                on_click=self.button_clicked,
+                                data="3",
                             ),
                             ElevatedButton(
                                 text="+",
                                 bgcolor=colors.ORANGE,
                                 color=colors.WHITE,
                                 expand=1,
+                                on_click=self.button_clicked,
+                                data="+",
                             ),
                         ]
                     ),
@@ -142,12 +168,16 @@ class CalcApp:
                                 bgcolor=colors.WHITE24,
                                 color=colors.WHITE,
                                 expand=2,
+                                on_click=self.button_clicked,
+                                data="0",
                             ),
                             ElevatedButton(
                                 text=".",
                                 bgcolor=colors.WHITE24,
                                 color=colors.WHITE,
                                 expand=1,
+                                on_click=self.button_clicked,
+                                data=".",
                             ),
                             ElevatedButton(
                                 text="=",
@@ -161,7 +191,22 @@ class CalcApp:
             ),
         )
 
-    def add_clicked(self, e):
+    def button_clicked(self, e):
+        if e.data == "AC":
+            self.result.value = "0"
+
+        elif e.data in ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."):
+            if self.result.value == "0" or self.new_operand == True:
+                self.result.value = e.data
+                self.new_operand = False
+            else:
+                self.result.value = self.result.value + e.data
+
+        elif e.data in ("+"):
+            self.operator = e.data
+            self.operand1 = float(self.result.value)
+            self.new_operand = True
+
         self.view.update()
 
 
