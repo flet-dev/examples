5ab60ba242ea486eeafd1d433612041b5bc50c61
Inesa Fitsner <inesa@appveyor.com>
2022-07-28 10:21:11 -0700

    Update page_window.py

--- python/controls/page/page_window.py
+++ python/controls/page/page_window.py
@@ -8,6 +8,7 @@ from flet import (
     Row,
     Switch,
     Text,
+    alignment,
     border,
     border_radius,
     colors,
@@ -50,9 +51,7 @@ def main(page: Page):
         minimize.disabled = not resizable.value
         page.update()
 
-    always_on_top = Switch(
-        label="Always on top", value=False, on_change=always_on_top_changed
-    )
+    always_on_top = Switch(value=False, on_change=always_on_top_changed)
 
     full_screen = Switch(
         label="Full screen", value=False, on_change=full_screen_changed
@@ -98,83 +97,94 @@ def main(page: Page):
     # add controls on Page
     page.add(
         Row(
-            alignment="spaceBetween",
             controls=[
-                Container(bgcolor=colors.AMBER_100, content=always_on_top),
-                Container(bgcolor=colors.AMBER_200, content=maximize),
-                Container(bgcolor=colors.AMBER_300, content=full_screen),
+                Container(
+                    expand=1,
+                    bgcolor=colors.LIGHT_BLUE_500,
+                    alignment=alignment.center,
+                    content=always_on_top,
+                ),
+                Container(expand=1, alignment=alignment.center, content=maximize),
+                Container(expand=1, alignment=alignment.center, content=full_screen),
             ],
         ),
         Row(
             alignment="spaceBetween",
             controls=[
-                Container(bgcolor=colors.AMBER_100, content=Text()),
-                Container(
-                    bgcolor=colors.AMBER_200,
-                    width=300,
-                    height=300,
-                    border=border.all(1, colors.BLACK),
-                    padding=padding.all(10),
-                    content=Column(
-                        alignment="spaceBetween",
-                        controls=[
-                            Row(
-                                alignment="center",
-                                controls=[
-                                    Container(
-                                        bgcolor=colors.LIGHT_BLUE_500,
-                                        border_radius=border_radius.all(30),
-                                        content=IconButton(
-                                            icon=icons.KEYBOARD_ARROW_UP,
-                                            on_click=move_up,
-                                        ),
-                                    ),
-                                ],
-                            ),
-                            Row(
+                Column(expand=1, controls=[Text()]),
+                Column(
+                    expand=1,
+                    controls=[
+                        Container(
+                            bgcolor=colors.AMBER_200,
+                            width=300,
+                            height=300,
+                            border=border.all(1, colors.BLACK),
+                            padding=padding.all(10),
+                            content=Column(
                                 alignment="spaceBetween",
                                 controls=[
-                                    Container(
-                                        bgcolor=colors.LIGHT_BLUE_500,
-                                        border_radius=border_radius.all(30),
-                                        content=IconButton(
-                                            icon=icons.KEYBOARD_ARROW_LEFT,
-                                            on_click=move_left,
-                                        ),
+                                    Row(
+                                        alignment="center",
+                                        controls=[
+                                            Container(
+                                                bgcolor=colors.LIGHT_BLUE_500,
+                                                border_radius=border_radius.all(30),
+                                                content=IconButton(
+                                                    icon=icons.KEYBOARD_ARROW_UP,
+                                                    on_click=move_up,
+                                                ),
+                                            ),
+                                        ],
                                     ),
-                                    Container(
-                                        bgcolor=colors.LIGHT_BLUE_500,
-                                        border_radius=border_radius.all(30),
-                                        content=IconButton(
-                                            icon=icons.KEYBOARD_ARROW_RIGHT,
-                                            on_click=move_right,
-                                        ),
+                                    Row(
+                                        alignment="spaceBetween",
+                                        controls=[
+                                            Container(
+                                                bgcolor=colors.LIGHT_BLUE_500,
+                                                border_radius=border_radius.all(30),
+                                                content=IconButton(
+                                                    icon=icons.KEYBOARD_ARROW_LEFT,
+                                                    on_click=move_left,
+                                                ),
+                                            ),
+                                            Container(
+                                                bgcolor=colors.LIGHT_BLUE_500,
+                                                border_radius=border_radius.all(30),
+                                                content=IconButton(
+                                                    icon=icons.KEYBOARD_ARROW_RIGHT,
+                                                    on_click=move_right,
+                                                ),
+                                            ),
+                                        ],
+                                    ),
+                                    Row(
+                                        alignment="center",
+                                        controls=[
+                                            Container(
+                                                bgcolor=colors.LIGHT_BLUE_500,
+                                                border_radius=border_radius.all(30),
+                                                content=IconButton(
+                                                    icon=icons.KEYBOARD_ARROW_DOWN,
+                                                    on_click=move_down,
+                                                ),
+                                            ),
+                                        ],
                                     ),
                                 ],
                             ),
-                            Row(
-                                alignment="center",
-                                controls=[
-                                    Container(
-                                        bgcolor=colors.LIGHT_BLUE_500,
-                                        border_radius=border_radius.all(30),
-                                        content=IconButton(
-                                            icon=icons.KEYBOARD_ARROW_DOWN,
-                                            on_click=move_down,
-                                        ),
-                                    ),
-                                ],
-                            ),
-                        ],
-                    ),
+                        )
+                    ],
                 ),
-                Container(bgcolor=colors.AMBER_300, content=resizable),
+                Column(expand=1, controls=[resizable]),
             ],
         ),
         Row(
-            alignment="spaceBetween",
+            # alignment="spaceBetween",
             controls=[
                 Container(
+                    expand=1,
+                    alignment=alignment.center,
                     bgcolor=colors.AMBER_100,
                     content=Column(
                         controls=[
@@ -185,8 +195,13 @@ def main(page: Page):
                         ],
                     ),
                 ),
-                Container(bgcolor=colors.AMBER_200, content=minimize),
-                Container(bgcolor=colors.AMBER_300, content=Text()),
+                Container(
+                    expand=1,
+                    bgcolor=colors.AMBER_200,
+                    content=minimize,
+                    alignment=alignment.center,
+                ),
+                Container(expand=1, bgcolor=colors.AMBER_300, content=Text()),
             ],
         ),
     )
