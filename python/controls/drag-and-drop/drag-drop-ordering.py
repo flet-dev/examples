b5aeb0f34b1b4d3476cfbbff7e5f612d65c2e0b3
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2022-08-18 15:54:31 -0700

    attempt at basic cross-list drag drop swap.

---
+++ python/controls/drag-and-drop/drag-drop-ordering.py
@@ -0,0 +1,136 @@
+import flet
+from flet import (
+    Page,
+    DragTarget,
+    Draggable,
+    Container,
+    Column,
+    Row,
+    Text,
+    TextButton,
+    Icon,
+    TextField,
+    Card,
+    icons,
+    border_radius,
+    border,
+    colors,
+    padding,
+)
+
+
+class ItemList():
+    def __init__(self, page, list_name, color):
+
+        self.page = page
+        self.list_name = list_name
+        self.items = []
+        self.new_item = TextField(label="new item name", width=200)
+        self.view = DragTarget(
+            group="items",
+            content=Container(
+                content=Column([
+                    self.new_item,
+                    TextButton(
+                        "add item", icon=icons.ADD, on_click=self.add_item),
+                    *self.items
+                ]),
+                border=border.all(2, colors.BLACK12),
+                border_radius=border_radius.all(15),
+                bgcolor=color,
+                padding=padding.all(20)
+            )
+        )
+
+    def add_item(self, e):
+        print("add click called")
+        new_item = Item(self, self.new_item.value)
+        self.view.content.content.controls.append(new_item.view)
+        self.items.append(new_item.view)
+        print("self.items: ", self.items)
+        self.page.update()
+        self.view.update()
+
+
+class Item():
+    def __init__(self, list, item_text):
+        self.list = list
+        self.item_text = item_text
+        self.card_item = Card(
+            content=Container(
+                content=Row([
+                    Icon(name=icons.CIRCLE_OUTLINED),
+                    Text(value=f"{self.item_text}")
+
+                ],
+                    alignment="start",
+                ),
+                width=200,
+                padding=10
+            ),
+            elevation=1
+        )
+        self.view = Draggable(
+            group="items",
+            content=DragTarget(
+                group="items",
+                content=self.card_item,
+                # content=Container(
+                #     content=Row([
+                #         Icon(name=icons.CIRCLE_OUTLINED),
+                #         Text(value=f"{self.item_text}")
+                #     ])
+                # ),
+                on_accept=self.drag_accept,
+                on_leave=self.drag_leave,
+                on_will_accept=self.drag_will_accept
+            )
+        )
+
+    def drag_accept(self, e):
+        # this is the item picked up
+        src = self.list.page.get_control(e.data)
+        swap = e.control.content.content
+        # this is the drag target, i.e. Item in the list
+        e.control.content.content = src.content.content
+        src.content.content = swap
+        e.control.update()
+        src.update
+
+    def drag_will_accept(self, e):
+        e.control.content.elevation = 2 if e.data == "true" else 1
+
+        e.control.update()
+
+    def drag_leave(self, e):
+        e.control.content.border = None
+        e.control.update()
+
+
+def main(page: Page):
+    page.title = "Drag and drop ordering"
+
+    page.add(
+
+        Row(
+            [
+                Draggable(
+                    group="items",
+                    content=ItemList(
+                        page, "List 1", colors.DEEP_ORANGE_400).view
+                ),
+                Draggable(
+                    group="items",
+                    content=ItemList(page, "List 2", colors.PINK_400).view
+                ),
+                Draggable(
+                    group="items",
+                    content=ItemList(page, "List 3", colors.CYAN_400).view
+                ),
+            ]
+        )
+
+    )
+
+
+flet.app(target=main)
