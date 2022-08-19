29742b5166d677b81704fd050921ed6d707c2c75
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2022-08-19 16:40:52 -0700

    data field on draggable interfering with page.get_control

--- python/controls/drag-and-drop/drag-drop-ordering.py
+++ python/controls/drag-and-drop/drag-drop-ordering.py
@@ -25,7 +25,7 @@ class ItemList():
 
         self.page = page
         self.list_name = list_name
-        self.items = []
+        self.items = Column([])
         self.new_item = TextField(label="new item name", width=200)
         self.view = DragTarget(
             group="list",
@@ -33,8 +33,8 @@ class ItemList():
                 content=Column([
                     self.new_item,
                     TextButton(
-                        "add item", icon=icons.ADD, on_click=self.add_item),
-                    *self.items
+                        "add item", icon=icons.ADD, on_click=self.add_item_handler),
+                    self.items
                 ]),
                 border=border.all(2, colors.BLACK12),
                 border_radius=border_radius.all(15),
@@ -43,16 +43,23 @@ class ItemList():
             )
         )
 
-    def add_item(self, e):
+    def add_item_handler(self, e):
+        self.add_item()
+
+    def add_item(self):
         new_item = Item(self, self.new_item.value)
-        self.view.content.content.controls.append(new_item.view)
-        self.items.append(new_item.view)
+        self.items.controls.append(new_item.view)
         self.page.update()
         self.view.update()
 
+    def remove_item(self, item: 'Item'):
+        self.items.controls.remove(item.view)
+        self.view.update()
+        pass
+
 
 class Item():
-    def __init__(self, list, item_text):
+    def __init__(self, list: ItemList, item_text):
         self.list = list
         self.item_text = item_text
         self.card_item = Card(
@@ -81,25 +88,28 @@ class Item():
                 # ),
                 on_accept=self.drag_accept,
                 on_leave=self.drag_leave,
-                on_will_accept=self.drag_will_accept
+                on_will_accept=self.drag_will_accept,
+                data=self.list
             )
+
         )
 
     def drag_accept(self, e):
         # this is the item picked up
         src = self.list.page.get_control(e.data)
-        print("src.content.content: ", src.content.content)
+        print("src.content.content: ", src)
+        # skip if item is dropped on itself
+        if (src.content.content == e.control.content):
+            return
+        print("e.control: ", e.control)
         # this is the drag target, i.e. Item in the list
-        swap = copy.copy(e.control.content)
-        print("swap: ", swap)
-        e.control.content = copy.copy(src.content.content)
-        src.content.content = swap
+        e.control.data.add_list(src)
+
         e.control.update()
         # src.update
 
     def drag_will_accept(self, e):
         # e.control.content.elevation = 2 if e.data == "true" else 1
-
         # e.control.update()
         pass
 
