37c5c0e2537e07897726ea733771e4fdb17d0387
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2022-08-22 16:14:26 -0700

    ...

--- python/controls/drag-and-drop/drag-drop-ordering.py
+++ python/controls/drag-and-drop/drag-drop-ordering.py
@@ -18,6 +18,7 @@ from flet import (
     icons,
     border_radius,
     border,
+    alignment,
     colors,
     padding,
 )
@@ -36,13 +37,13 @@ class ItemList():
                 bgcolor=colors.BLACK26,
                 #border=border.all(2, colors.BLACK26),
                 border_radius=border_radius.all(30),
-                height=5,
+                height=3,
                 width=200,
-                opacity=0.0
+                opacity=0.0,
+                data=-1
                 # expand=True
-
             )
-        ])
+        ], spacing=1, tight=True)
         self.item_name = TextField(label="new item name", width=200)
         self.view = DragTarget(
             group="items",
@@ -65,18 +66,40 @@ class ItemList():
     def add_item_handler(self, e):
         self.add_item()
 
-    def add_item(self, item: str = None):
-        new_item = Item(self, item) if item else Item(
-            self, self.item_name.value)
-        # self.items.controls.append(new_item.view)
-        self.items.controls.insert(-1, new_item.view)
+    def add_item(self, item: str = None, index: int = None):
+        new_item = Item(self, item, (len(self.items.controls) - 1)) if item else Item(
+            self, self.item_name.value, (len(self.items.controls) - 1))
+
+        controls_to_add = Column([
+            Container(
+                bgcolor=colors.BLACK26,
+                border_radius=border_radius.all(30),
+                height=3,
+                alignment=alignment.center_right,
+                width=200,
+                opacity=0.0
+            ),
+            new_item.view
+        ])
+        # insert
+        if (index):
+            self.items.controls.insert(index, controls_to_add)
+        # add new
+        else:
+            self.items.controls.insert(-1, controls_to_add)
         self.item_name.value = ""
         print("self.items: ", self.items.controls)
         self.page.update()
         self.view.update()
 
+    def set_indicator_opacity(self, target, opacity):
+        #print("target: ", target)
+        #index = [x.content for x in self.items.controls].index(target)
+        self.items.controls[target].controls[0].opacity = opacity
+        self.view.update()
+
     def remove_item(self, item):
-        print("item from remove_item: ", item)
+        #print("item from remove_item: ", item)
         self.items.controls.remove(item)
         self.view.update()
 
@@ -90,8 +113,9 @@ class ItemList():
 
 
 class Item():
-    def __init__(self, list: ItemList, item_text):
+    def __init__(self, list: ItemList, item_text: str, item_index: int):
         self.list = list
+        self.item_index = item_index
         self.item_text = item_text
         self.card_item = Card(
             content=Container(
@@ -102,7 +126,7 @@ class Item():
                     alignment="start",
                 ),
                 width=200,
-                padding=10
+                padding=7
             ),
             elevation=1,
             data=self.list
@@ -116,7 +140,7 @@ class Item():
                 on_leave=self.drag_leave,
                 on_will_accept=self.drag_will_accept,
                 # wy can't this be added to this component?
-                # data=self.item_text
+                # data=self.list
             ),
             data=self
 
@@ -135,10 +159,13 @@ class Item():
 
         # this is the drag target, i.e. Item in the list (DragTarget)
         print("e.control: ", e.control)
-
-        self.list.add_item(src.data.item_text)
+        #index = [x.content for x in self.list.items.controls].index(e.control)
+        print("self.item_index: ", self.item_index)
+        self.list.add_item(src.data.item_text, self.item_index)
         # remove from the list to which draggable belongs
         src.data.list.remove_item(src)
+        #self.list.set_indicator_opacity(e.control, 0.0)
+        self.list.set_indicator_opacity(self.item_index, 0.0)
         e.control.content.elevation = 1
         e.control.update()
         # self.list.update()
@@ -146,10 +173,15 @@ class Item():
     def drag_will_accept(self, e):
         # e.control == self.view.content(dragtarget)
         # therefore no way to check if target and draggable are the same control.
+        self.list.set_indicator_opacity(self.item_index, 1.0)
+        #self.list.set_indicator_opacity(e.control, 1.0)
         e.control.content.elevation = 20 if e.data == "true" else 1
         e.control.update()
 
     def drag_leave(self, e):
+        # index = self.list.items.controls.index(self.view)
+        # self.list.items.controls[index - 1].opacity = 0.0
+        self.list.set_indicator_opacity(e.control, 0.0)
         e.control.content.elevation = 1
         e.control.update()
 
