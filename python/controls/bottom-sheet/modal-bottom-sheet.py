04ce757fa4121c4f3f897efd32123f46466ce153
Feodor Fitsner <feodor@appveyor.com>
2022-11-13 15:55:03 -0800

    Controls s3 (#41)
    * ResponsiveRow and variable wight font examples
    
    * Do not install Flutter on Ubuntu images
    
    * NavigationBar and Tooltip examples
    
    * Create markdown-code-highlight.py
    
    * import flet as ft

---
+++ python/controls/bottom-sheet/modal-bottom-sheet.py
@@ -0,0 +1,34 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    def bs_dismissed(e):
+        print("Dismissed!")
+
+    def show_bs(e):
+        bs.open = True
+        bs.update()
+
+    def close_bs(e):
+        bs.open = False
+        bs.update()
+
+    bs = ft.BottomSheet(
+        ft.Container(
+            ft.Column(
+                [
+                    ft.Text("This is sheet's content!"),
+                    ft.ElevatedButton("Close bottom sheet", on_click=close_bs),
+                ],
+                tight=True,
+            ),
+            padding=10,
+        ),
+        open=True,
+        on_dismiss=bs_dismissed,
+    )
+    page.overlay.append(bs)
+    page.add(ft.ElevatedButton("Display bottom sheet", on_click=show_bs))
+
+
+ft.app(target=main)
