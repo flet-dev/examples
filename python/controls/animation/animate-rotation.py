fb453cde0bfb73d03edf4c8e1924785a0a70ec11
Feodor Fitsner <feodor@appveyor.com>
2022-11-24 11:31:22 -0800

    AlertDialog, Animations to enums

--- python/controls/animation/animate-rotation.py
+++ python/controls/animation/animate-rotation.py
@@ -1,10 +1,9 @@
-import flet
-from flet import Container, ElevatedButton, Page, Stack, alignment, animation, transform
+import flet as ft
 
 
-def main(page: Page):
+def main(page: ft.Page):
 
-    c1 = Container(
+    c1 = ft.Container(
         width=140,
         height=100,
         left=200,
@@ -15,13 +14,13 @@ def main(page: Page):
         animate_rotation=1000,
     )
 
-    c2 = Container(
+    c2 = ft.Container(
         width=100,
         height=70,
         bgcolor="blue",
         border_radius=5,
-        rotate=transform.Rotate(-1, alignment=alignment.center_left),
-        animate_rotation=animation.Animation(duration=300),
+        rotate=ft.transform.Rotate(-1, alignment=ft.alignment.center_left),
+        animate_rotation=ft.animation.Animation(duration=300),
     )
 
     def animate(e):
@@ -30,9 +29,9 @@ def main(page: Page):
         page.update()
 
     page.add(
-        Stack([c1, c2], width=600, height=600),
-        ElevatedButton("Animate!", on_click=animate),
+        ft.Stack([c1, c2], width=600, height=600),
+        ft.ElevatedButton("Animate!", on_click=animate),
     )
 
 
-flet.app(target=main)
+ft.app(target=main)
---
+++ python/controls/animation/animate-scale-docs.py
@@ -0,0 +1,29 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+
+    c = ft.Container(
+        width=100,
+        height=100,
+        bgcolor="blue",
+        border_radius=5,
+        scale=ft.transform.Scale(scale=1),
+        animate_scale=ft.animation.Animation(600, ft.AnimationCurve.BOUNCE_OUT),
+    )
+
+    def animate(e):
+        # c1.rotate = 1
+        c.scale = 2
+        page.update()
+
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.spacing = 30
+    page.add(
+        c,
+        ft.ElevatedButton("Animate!", on_click=animate),
+    )
+
+
+ft.app(target=main)
