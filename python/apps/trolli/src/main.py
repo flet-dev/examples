156df3aadcf7b4191d53aef0ae184c9e0fe1411f
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-01-11 16:53:38 -0800

    start adding auth logic.

--- python/apps/trolli/src/main.py
+++ python/apps/trolli/src/main.py
@@ -1,6 +1,9 @@
 from board import Board
+import os
 import flet
 from flet.buttons import RoundedRectangleBorder
+from flet.auth.providers.auth0_oauth_provider import Auth0OAuthProvider
+from flet.security import decrypt, encrypt
 from flet import (
     UserControl,
     View,
@@ -61,12 +64,35 @@ class TrelloApp(UserControl):
             ],
         )
         self.page.appbar = self.appbar
+        self.encryption_key = os.getenv("TROLLI_ENCRYPTION_KEY")
+        self.provider = Auth0OAuthProvider(
+            domain="dev-j3wnirdjarxj51uz.us.auth0.com",
+            client_id=os.getenv("AUTH0_CLIENT_ID"),
+            client_secret=os.getenv("AUTH0_CLIENT_SECRET"),
+            redirect_url="http://localhost:8088/api/oauth/redirect",
+        )
+
+        page.on_login = self.on_login
         self.page.update()
+
+    # def login_click(self, e):
 
     def build(self):
         self.layout = AppLayout(self, self.page, self.store,
                                 tight=True, expand=True, vertical_alignment="start")
         return self.layout
+
+    def on_login(self, e):
+        if e.error:
+            raise Exception(e.error)
+
+        # save token in a client storage
+        jt = self.page.auth.token.to_json()
+        ejt = encrypt(jt, self.encryption_key)
+        self.page.client_storage.set("trolli_token", ejt)
+
+        # change login button text and Workspace visibility
+        self.page.update()
 
     def initialize(self):
         self.page.views.clear()
@@ -89,37 +115,43 @@ class TrelloApp(UserControl):
 
     def login(self, e):
 
-        def close_dlg(e):
-            if user_name.value == "" or password.value == "":
-                user_name.error_text = "Please provide username"
-                password.error_text = "Please provide password"
-                self.page.update()
-                return
-            else:
-                user = User(user_name.value, password.value)
-                if user not in self.store.get_users():
-                    self.store.add_user(user)
-                self.user = user_name.value
-                self.page.client_storage.set("current_user", user_name.value)
+        # def close_dlg(e):
+        #     if user_name.value == "" or password.value == "":
+        #         user_name.error_text = "Please provide username"
+        #         password.error_text = "Please provide password"
+        #         self.page.update()
+        #         return
+        #     else:
+        #         user = User(user_name.value, password.value)
+        #         if user not in self.store.get_users():
+        #             self.store.add_user(user)
+        #         self.user = user_name.value
+        #         self.page.client_storage.set("current_user", user_name.value)
 
-            dialog.open = False
-            self.appbar_items[0] = PopupMenuItem(
-                text=f"{self.page.client_storage.get('current_user')}'s Profile")
-            self.page.update()
-        user_name = TextField(label="User name")
-        password = TextField(label="Password", password=True)
-        dialog = AlertDialog(
-            title=Text("Please enter your login credentials"),
-            content=Column([
-                user_name,
-                password,
-                ElevatedButton(text="Login", on_click=close_dlg),
-            ], tight=True),
-            on_dismiss=lambda e: print("Modal dialog dismissed!"),
-        )
-        self.page.dialog = dialog
-        dialog.open = True
-        self.page.update()
+        #     dialog.open = False
+        #     self.appbar_items[0] = PopupMenuItem(
+        #         text=f"{self.page.client_storage.get('current_user')}'s Profile")
+        #     self.page.update()
+        # user_name = TextField(label="User name")
+        # password = TextField(label="Password", password=True)
+        # dialog = AlertDialog(
+        #     title=Text("Please enter your login credentials"),
+        #     content=Column([
+        #         user_name,
+        #         password,
+        #         ElevatedButton(text="Login", on_click=close_dlg),
+        #     ], tight=True),
+        #     on_dismiss=lambda e: print("Modal dialog dismissed!"),
+        # )
+        # self.page.dialog = dialog
+        # dialog.open = True
+        # self.page.update()
+        saved_token = None
+        ejt = self.page.client_storage.get("trolli.token")
+        if ejt:
+            saved_token = decrypt(ejt, self.encryption_key)
+        if e is not None or saved_token is not None:
+            self.page.login(self.provider, saved_token=saved_token)
 
     def route_change(self, e):
         troute = TemplateRoute(self.page.route)
@@ -196,8 +228,10 @@ if __name__ == "__main__":
         }
         page.bgcolor = colors.BLUE_GREY_200
         app = TrelloApp(page, InMemoryStore())
+
         page.add(app)
         page.update()
         app.initialize()
 
-    flet.app(target=main, assets_dir="../assets")
+    flet.app(target=main, port=8088,
+             assets_dir="../assets", view=flet.WEB_BROWSER)
