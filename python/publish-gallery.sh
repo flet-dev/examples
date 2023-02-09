589d209e771c78074a0e35ecefead7abdae6d766
Feodor Fitsner <feodor@appveyor.com>
2023-02-09 13:35:59 -0800

    Create publish-gallery.sh

---
+++ python/publish-gallery.sh
@@ -0,0 +1,3 @@
+pip install flet --pre
+flet --version
+flet publish python/apps/todo/todo.py --distpath python/dist/todo --base-url todo --pre --app-name "Flet To-Do" --app-description "A classic To-Do app inspired by TodoMVC project."
