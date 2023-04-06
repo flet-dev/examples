2ada69efab1d97a5c39085c1b0df7bf9807e6a45
Feodor Fitsner <feodor@appveyor.com>
2023-04-06 15:50:40 -0700

    Flet 0.5.0 examples (#72)
    * LineChart monthly sales
    
    * PieChart examples
    
    * Container blur and shadow examples

---
+++ python/controls/charts/piechart-sample-2.py
@@ -0,0 +1,67 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    normal_radius = 50
+    hover_radius = 60
+    normal_title_style = ft.TextStyle(
+        size=16, color=ft.colors.WHITE, weight=ft.FontWeight.BOLD
+    )
+    hover_title_style = ft.TextStyle(
+        size=22,
+        color=ft.colors.WHITE,
+        weight=ft.FontWeight.BOLD,
+        shadow=ft.BoxShadow(blur_radius=2, color=ft.colors.BLACK54),
+    )
+
+    def on_chart_event(e: ft.PieChartEvent):
+        for idx, section in enumerate(chart.sections):
+            if idx == e.section_index:
+                section.radius = hover_radius
+                section.title_style = hover_title_style
+            else:
+                section.radius = normal_radius
+                section.title_style = normal_title_style
+        chart.update()
+
+    chart = ft.PieChart(
+        sections=[
+            ft.PieChartSection(
+                40,
+                title="40%",
+                title_style=normal_title_style,
+                color=ft.colors.BLUE,
+                radius=normal_radius,
+            ),
+            ft.PieChartSection(
+                30,
+                title="30%",
+                title_style=normal_title_style,
+                color=ft.colors.YELLOW,
+                radius=normal_radius,
+            ),
+            ft.PieChartSection(
+                15,
+                title="15%",
+                title_style=normal_title_style,
+                color=ft.colors.PURPLE,
+                radius=normal_radius,
+            ),
+            ft.PieChartSection(
+                15,
+                title="15%",
+                title_style=normal_title_style,
+                color=ft.colors.GREEN,
+                radius=normal_radius,
+            ),
+        ],
+        sections_space=0,
+        center_space_radius=40,
+        on_chart_event=on_chart_event,
+        expand=True,
+    )
+
+    page.add(chart)
+
+
+ft.app(main)
---
+++ python/controls/charts/piechart-sample-3.py
@@ -0,0 +1,87 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    normal_radius = 100
+    hover_radius = 110
+    normal_title_style = ft.TextStyle(
+        size=12, color=ft.colors.WHITE, weight=ft.FontWeight.BOLD
+    )
+    hover_title_style = ft.TextStyle(
+        size=16,
+        color=ft.colors.WHITE,
+        weight=ft.FontWeight.BOLD,
+        shadow=ft.BoxShadow(blur_radius=2, color=ft.colors.BLACK54),
+    )
+    normal_badge_size = 40
+    hover_badge_size = 50
+
+    def badge(icon, size):
+        return ft.Container(
+            ft.Icon(icon),
+            width=size,
+            height=size,
+            border=ft.border.all(1, ft.colors.BROWN),
+            border_radius=size / 2,
+            bgcolor=ft.colors.WHITE,
+        )
+
+    def on_chart_event(e: ft.PieChartEvent):
+        for idx, section in enumerate(chart.sections):
+            if idx == e.section_index:
+                section.radius = hover_radius
+                section.title_style = hover_title_style
+            else:
+                section.radius = normal_radius
+                section.title_style = normal_title_style
+        chart.update()
+
+    chart = ft.PieChart(
+        sections=[
+            ft.PieChartSection(
+                40,
+                title="40%",
+                title_style=normal_title_style,
+                color=ft.colors.BLUE,
+                radius=normal_radius,
+                badge=badge(ft.icons.AC_UNIT, normal_badge_size),
+                badge_position=0.98,
+            ),
+            ft.PieChartSection(
+                30,
+                title="30%",
+                title_style=normal_title_style,
+                color=ft.colors.YELLOW,
+                radius=normal_radius,
+                badge=badge(ft.icons.ACCESS_ALARM, normal_badge_size),
+                badge_position=0.98,
+            ),
+            ft.PieChartSection(
+                15,
+                title="15%",
+                title_style=normal_title_style,
+                color=ft.colors.PURPLE,
+                radius=normal_radius,
+                badge=badge(ft.icons.APPLE, normal_badge_size),
+                badge_position=0.98,
+            ),
+            ft.PieChartSection(
+                15,
+                title="15%",
+                title_style=normal_title_style,
+                color=ft.colors.GREEN,
+                radius=normal_radius,
+                badge=badge(ft.icons.PEDAL_BIKE, normal_badge_size),
+                badge_position=0.98,
+            ),
+        ],
+        sections_space=0,
+        center_space_radius=0,
+        on_chart_event=on_chart_event,
+        expand=True,
+    )
+
+    page.add(chart)
+
+
+ft.app(main)
