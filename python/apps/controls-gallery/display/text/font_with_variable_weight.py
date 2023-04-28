70abbcf01ea2ddbd8a4a9d0e6f9ee5e25320e561
InesaFitsner <inesa@appveyor.com>
2023-04-28 12:57:12 -0700

    fixed Text font weight example

--- python/apps/controls-gallery/display/text/font_with_variable_weight.py
+++ python/apps/controls-gallery/display/text/font_with_variable_weight.py
@@ -3,29 +3,67 @@ import flet as ft
 name = "Font with variable weight"
 
 def example():
-    ft.page.fonts = {
-        "RobotoSlab": "https://github.com/google/fonts/raw/main/apache/robotoslab/RobotoSlab%5Bwght%5D.ttf"
-    }
 
-    t = ft.Text(
-        "This is rendered with Roboto Slab",
-        size=30,
-        font_family="RobotoSlab",
-        weight=ft.FontWeight.W_100,
-    )
+    class Example(ft.Column):
+        def __init__(self):
+            super().__init__()
+            self.t = ft.Text(
+                "This is rendered with Roboto Slab",
+                size=30,
+                font_family="RobotoSlab",
+                weight=ft.FontWeight.W_100,)
+            self.controls = [
+                self.t,
+                ft.Slider(
+                    min=100,
+                    max=900,
+                    divisions=8,
+                    label="{value}",
+                    width=500,
+                    on_change=self.width_changed,
+                ),
+                ]
 
-    def width_changed(e):
-        t.weight = f"w{int(e.control.value)}"
-        t.update()
+        # happens when example is added to the page (when user chooses the Text control from the grid)
+        def did_mount(self):
+            self.page.fonts["RobotoSlab"] = "RobotoSlab[wght].ttf"
+            self.page.update()
+        
+        def width_changed(self, e):
+            self.t.weight = f"w{int(e.control.value)}"
+            self.t.update()
+
     
-    return ft.Column(controls=[
-        t,
-        ft.Slider(
-            min=100,
-            max=900,
-            divisions=8,
-            label="{value}",
-            width=500,
-            on_change=width_changed,
-        )
-    ])
+    example = Example()
+    
+    return example
+
+
+# import flet as ft
+
+# name = "Autoplay Audio"
+
+# def example():
+#     class Example(ft.Column):
+#         def __init__(self):
+#             super().__init__()
+#             self.audio1 = ft.Audio(
+#                 src="https://luan.xyz/files/audio/ambient_c_motion.mp3", autoplay=True)
+            
+#             self.controls = [ft.Text("This is an app with background audio. Note: this example doesn't work in Safari browser."),
+#         ft.ElevatedButton("Stop playing", on_click=lambda _: self.audio1.pause())]
+            
+
+#         # happens when example is added to the page (when user chooses the Audio control from the grid)
+#         def did_mount(self):
+#             self.page.overlay.append(self.audio1)
+#             self.page.update()
+        
+#         # happens when example is removed from the page (when user chooses different control group on the navigation rail)
+#         def will_unmount(self):
+#             self.page.overlay.remove(self.audio1)
+#             self.page.update()
+  
+#     audio_example = Example()
+    
+#     return audio_example
