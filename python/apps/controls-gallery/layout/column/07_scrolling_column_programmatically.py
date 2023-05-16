f50f07a87a087b68e8cb80e8e4ae23a1d8ea7765
InesaFitsner <inesa@appveyor.com>
2023-05-16 12:42:05 -0700

    Update 07_scrolling_column_programmatically.py

--- python/apps/controls-gallery/layout/column/07_scrolling_column_programmatically.py
+++ python/apps/controls-gallery/layout/column/07_scrolling_column_programmatically.py
@@ -7,7 +7,8 @@ def example():
     cl = ft.Column(
         spacing=10,
         height=200,
-        width=float("inf"),
+        #width=float("inf"),
+        width=200,
         scroll=ft.ScrollMode.ALWAYS,
     )
     for i in range(0, 100):
@@ -22,8 +23,8 @@ def example():
     def scroll_to_end(e):
         cl.scroll_to(offset=-1, duration=2000, curve=ft.AnimationCurve.EASE_IN_OUT)
 
-    def scroll_to_key(e):
-        cl.scroll_to(key="20", duration=1000)
+    # def scroll_to_key(e):
+    #     cl.scroll_to(key="20", duration=1000)
 
     def scroll_to_delta(e):
         cl.scroll_to(delta=40, duration=200)
@@ -41,7 +42,7 @@ def example():
                 ft.ElevatedButton("Scroll to end", on_click=scroll_to_end),
             ]
             ),
-            ft.ElevatedButton("Scroll to key '20'", on_click=scroll_to_key),
+            #ft.ElevatedButton("Scroll to key '20'", on_click=scroll_to_key),
             ft.Row(
             [
                 ft.ElevatedButton("Scroll -40", on_click=scroll_to_minus_delta),
