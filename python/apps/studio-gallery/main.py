19e0a7176ad38e766e71fa90fe86935b204fc3f7
InesaFitsner <inesa@appveyor.com>
2023-06-09 13:38:57 -0700

    AppButton class properties

--- python/apps/studio-gallery/main.py
+++ python/apps/studio-gallery/main.py
@@ -1,51 +1,37 @@
 import flet as ft
 import counter
 
-examples = {"counter": counter.example}
-
-
-class Example:
-    def __init__(self, name, control):
-        self.name = name
-        self.control = control
-
-
-# examples = [Example("Counter", counter.example())]
-
 
 class AppButton(ft.Container):
-    def __init__(self, name):
+    def __init__(self, name, view):
         super().__init__()
         self.height = 70
         self.padding = 10
-        # self.ref_to_control = ref_to_control
+        self.view = view
         self.border_radius = 5
         self.bgcolor = ft.colors.SURFACE_VARIANT
         self.content = ft.Row([ft.Text(name)])
         self.on_click = self.app_button_clicked
-        self.data = name
+        self.name = name
 
     def app_button_clicked(self, e):
-        if e.control.data == "Counter":
-            e.control.page.views.append(
-                ft.View(
-                    controls=[
-                        ft.AppBar(title=ft.Text("Counter app")),
-                        examples["counter"](),
-                    ]
-                )
-                # ft.View("/", [ft.Text("Counter text")])
+        e.control.page.views.append(
+            ft.View(
+                controls=[
+                    ft.AppBar(title=ft.Text(f"{e.control.name} app")),
+                    e.control.view,
+                ]
             )
-            print(len(e.control.page.views))
-            e.control.page.update()
+        )
+        e.control.page.update()
 
 
 def main(page: ft.Page):
-    counter = AppButton("Counter")
+    counter_button = AppButton("Counter", view=counter.example())
 
-    to_do = AppButton("To-Do")
+    # to_do = AppButton("To-Do")
 
-    page.add(ft.Column(controls=[counter, to_do]))
+    page.add(ft.Column(controls=[counter_button]))
 
     def view_pop(view):
         page.views.pop()
