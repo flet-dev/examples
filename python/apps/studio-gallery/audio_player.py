ab20e9237f124f8940f0a04df76bd3c272d59e70
InesaFitsner <inesa@appveyor.com>
2023-06-14 13:45:00 -0700

    TrackCanvas

--- python/apps/studio-gallery/audio_player.py
+++ python/apps/studio-gallery/audio_player.py
@@ -2,10 +2,17 @@ import flet as ft
 
 
 def example():
+    import flet.canvas as cv
+
     url = "https://github.com/mdn/webaudio-examples/blob/main/audio-analyser/viper.mp3?raw=true"
 
+    def convertMillis(millis):
+        seconds = int(millis / 1000) % 60
+        minutes = int(millis / (1000 * 60)) % 60
+        return f"{minutes}:{seconds}"
+
     class Track(ft.GestureDetector):
-        def __init__(self):
+        def __init__(self, audio):
             super().__init__()
             self.content = ft.Stack(
                 expand=True,
@@ -14,14 +21,65 @@ def example():
                     ft.Container(expand=True, border_radius=3, bgcolor=ft.colors.BLACK)
                 ],
             )
-            self.on_tap = self.seek
+            self.audio = audio
+            # self.duration = duration
+            self.on_pan_start = self.find_position
             self.on_hover = self.change_cursor
 
-        def seek(self, e):
-            print("Seek")
+        def find_position(self, e: ft.DragStartEvent):
+            print(f"Total track width: {self.content.width}")
+            print(f"Total track duration: {convertMillis(self.audio.get_duration())}")
+            print(
+                f"Position: {convertMillis(self.audio.get_duration()*e.local_x/e.control.page.width)}"
+            )
+            print(e.control.content.controls[0].width)
 
-        def change_cursor(self, e):
-            print("Change cursor")
+        def change_cursor(self, e: ft.HoverEvent):
+            e.control.mouse_cursor = ft.MouseCursor.CLICK
+            e.control.update()
+
+    class TrackCanvas(ft.GestureDetector):
+        def __init__(self, audio):
+            super().__init__()
+            self.content = ft.Container(
+                content=cv.Canvas(
+                    on_resize=self.canvas_resized,
+                    shapes=[
+                        cv.Rect(
+                            x=0,
+                            y=0,
+                            height=5,
+                            border_radius=3,
+                            paint=ft.Paint(color="black"),
+                            width=100,
+                        )
+                    ],
+                ),
+                height=10,
+                width=float("inf"),
+            )
+            self.audio = audio
+            # self.duration = duration
+            self.on_pan_start = self.find_position
+            self.on_hover = self.change_cursor
+
+        def canvas_resized(self, e: cv.CanvasResizeEvent):
+            print("On resize:", e.width, e.height)
+            self.track_width = e.width
+            e.control.shapes[0].width = e.width
+            e.control.update()
+
+        def find_position(self, e: ft.DragStartEvent):
+            print(f"Total track width: {self.content.width}")
+            print(f"Total track duration: {convertMillis(self.audio.get_duration())}")
+            print(
+                f"Position: {convertMillis(self.audio.get_duration()*e.local_x/e.control.page.width)}"
+            )
+            # print(e.control.content.controls[0].width)
+
+        def change_cursor(self, e: ft.HoverEvent):
+            e.control.mouse_cursor = ft.MouseCursor.CLICK
+            e.control.update()
 
     class Example(ft.Column):
         def __init__(self):
@@ -37,9 +95,11 @@ def example():
                 on_state_changed=lambda e: print("State changed:", e.data),
                 on_seek_complete=lambda _: print("Seek complete"),
             )
-            self.track = Track()
+            self.track_canvas = TrackCanvas(audio=self.audio1)
+            self.track = Track(audio=self.audio1)
             self.controls = [
-                self.track,
+                self.track_canvas,
+                # self.track,
                 ft.ElevatedButton("Play", on_click=lambda _: self.audio1.play()),
                 ft.ElevatedButton("Pause", on_click=lambda _: self.audio1.pause()),
                 ft.ElevatedButton("Resume", on_click=lambda _: self.audio1.resume()),
