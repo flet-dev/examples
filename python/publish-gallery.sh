11b2f8e5c325db65ed30620da1c3b2bff197f02d
Feodor Fitsner <feodor@appveyor.com>
2023-09-22 08:48:21 -0700

    Do not publish todo with --pre

--- python/publish-gallery.sh
+++ python/publish-gallery.sh
@@ -1,6 +1,6 @@
 pip install flet
 flet --version
-flet publish python/apps/todo/todo.py --distpath python/dist/todo --base-url todo --pre --app-name "Flet To-Do" --app-description "A classic To-Do app inspired by TodoMVC project."
+flet publish python/apps/todo/todo.py --distpath python/dist/todo --base-url todo --app-name "Flet To-Do" --app-description "A classic To-Do app inspired by TodoMVC project."
 flet publish python/apps/icons-browser/main.py --distpath python/dist/icons-browser --base-url icons-browser --pre --app-name "Flet Icons Browser" --app-description "Quickly search through icons collection to use in your app."
 flet publish python/tutorials/calc/calc.py --distpath python/dist/calculator --base-url calculator --pre --app-name "Calculator" --app-description "A simple calculator app written in Flet."
 flet publish python/tutorials/solitaire/solitaire-final-part1/main.py --distpath python/dist/solitaire --base-url solitaire --assets assets --pre --app-name "Solitaire" --app-description "Learn how to handle gestures and position controls on a page."
