22754e7f4f23a31223005620f92b7a1e4feb4d47
InesaFitsner <inesa@appveyor.com>
2023-09-26 10:57:26 -0700

    Update 01_basic_elevatedbuttons.py
    added code for example to be run on its own

--- python/apps/controls-gallery/buttons/elevatedbutton/01_basic_elevatedbuttons.py
+++ python/apps/controls-gallery/buttons/elevatedbutton/01_basic_elevatedbuttons.py
@@ -2,9 +2,22 @@ import flet as ft
 
 name = "Basic ElevatedButtons"
 
+
 def example():
-    
-    return ft.Column(controls=[
-        ft.ElevatedButton(text="Elevated button"),
-        ft.ElevatedButton("Disabled button", disabled=True)
-    ])
+    return ft.Column(
+        controls=[
+            ft.ElevatedButton(text="Elevated button"),
+            ft.ElevatedButton("Disabled button", disabled=True),
+        ]
+    )
+
+
+def main(page: ft.Page):
+    page.title = name
+    page.window_width = 390
+    page.window_height = 844
+    page.add(example())
+
+
+if __name__ == "__main__":
+    ft.app(target=main)
