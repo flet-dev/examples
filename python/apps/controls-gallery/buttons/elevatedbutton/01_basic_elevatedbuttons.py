5171c6352cf06a080c14ec175f4d27423dfe23c3
InesaFitsner <inesa@appveyor.com>
2023-09-26 12:47:28 -0700

    Source code for gallery (#94)
    * Update 01_basic_elevatedbuttons.py
    
    added code for example to be run on its own
    
    * file_name for example_item
    
    * file_name for example_item in gallerydata
    
    * removed print
    
    * removed code related to source code dialog

--- python/apps/controls-gallery/buttons/elevatedbutton/01_basic_elevatedbuttons.py
+++ python/apps/controls-gallery/buttons/elevatedbutton/01_basic_elevatedbuttons.py
@@ -2,9 +2,11 @@ import flet as ft
 
 name = "Basic ElevatedButtons"
 
+
 def example():
-    
-    return ft.Column(controls=[
-        ft.ElevatedButton(text="Elevated button"),
-        ft.ElevatedButton("Disabled button", disabled=True)
-    ])
+    return ft.Column(
+        controls=[
+            ft.ElevatedButton(text="Elevated button"),
+            ft.ElevatedButton("Disabled button", disabled=True),
+        ]
+    )
--- python/apps/controls-gallery/main.py
+++ python/apps/controls-gallery/main.py
@@ -69,9 +69,14 @@ async def main(page: ft.Page):
                                         weight=ft.FontWeight.W_500,
                                     ),
                                     ft.IconButton(
-                                        icon=ft.icons.CODE,
-                                        on_click=show_code,
-                                        data=example,
+                                        content=ft.Image(
+                                            src="github-mark.svg",
+                                            width=24,
+                                            height=24,
+                                            color=ft.colors.ON_SURFACE,
+                                        ),
+                                        url=f"https://github.com/flet-dev/examples/blob/main/python/apps/controls-gallery/{example.file_name}",
+                                        url_target="_blank",
                                     ),
                                 ],
                             ),
@@ -127,15 +132,6 @@ async def main(page: ft.Page):
     async def grid_item_clicked(e):
         route = f"{page.route}/{e.control.data.id}"
         await page.go_async(route)
-
-    async def show_code(e):
-        dlg.open = True
-        code_example_name.value = e.control.data.name
-        code_text = ft.Text(value=e.control.data.source_code, selectable=True)
-
-        dlg.content = ft.Column(controls=[code_text], scroll="always")
-        dlg.data = e.control.data.source_code
-        await page.update_async()
 
     def get_destinations():
         destinations = []
@@ -224,7 +220,6 @@ async def main(page: ft.Page):
 
     control_name = ft.Text(style=ft.TextThemeStyle.HEADLINE_MEDIUM)
     control_description = ft.Text(style=ft.TextThemeStyle.BODY_MEDIUM)
-    # listview = ft.ListView(expand=True, spacing=10, padding=0, auto_scroll=False)
     listview = ft.Column(expand=True, spacing=10, scroll=ft.ScrollMode.AUTO)
 
     examples = ft.Column(
@@ -245,32 +240,6 @@ async def main(page: ft.Page):
             )
         ],
     )
-
-    async def copy_source_code_to_clipboard(e):
-        source_code = dlg.data
-        await page.set_clipboard_async(source_code)
-        await page.show_snack_bar_async(
-            ft.SnackBar(ft.Text(f"Example source code copied to clipboard"), open=True)
-        )
-
-    async def close_dlg(e):
-        dlg.open = False
-        await page.update_async()
-
-    code_example_name = ft.Text("Example")
-
-    dlg = ft.AlertDialog(
-        title=code_example_name,
-        actions=[
-            ft.FilledButton(
-                "Copy to clipboard", on_click=copy_source_code_to_clipboard
-            ),
-            ft.TextButton("Close", on_click=close_dlg),
-        ],
-        actions_alignment=ft.MainAxisAlignment.END,
-    )
-
-    page.dialog = dlg
 
     page.theme_mode = ft.ThemeMode.LIGHT
 
