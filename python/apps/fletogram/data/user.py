85f273d785eca053a00a561203319ae1f72a57b0
InesaFitsner <inesa@appveyor.com>
2024-02-12 14:13:37 -0800

    refactor

--- python/apps/fletogram/data/user.py
+++ python/apps/fletogram/data/user.py
@@ -24,27 +24,3 @@ class User(ft.ListTile):
             color=ft.colors.WHITE,
             bgcolor=get_avatar_color(self.display_name),
         )
-
-    def get_initials(self, user_name: str):
-        if user_name:
-            return user_name[:1].capitalize()
-        else:
-            return "Unknown"  # or any default value you prefer
-
-    def get_avatar_color(self, user_name: str):
-        colors_lookup = [
-            ft.colors.AMBER,
-            ft.colors.BLUE,
-            ft.colors.BROWN,
-            ft.colors.CYAN,
-            ft.colors.GREEN,
-            ft.colors.INDIGO,
-            ft.colors.LIME,
-            ft.colors.ORANGE,
-            ft.colors.PINK,
-            ft.colors.PURPLE,
-            ft.colors.RED,
-            ft.colors.TEAL,
-            ft.colors.YELLOW,
-        ]
-        return colors_lookup[hash(user_name) % len(colors_lookup)]
