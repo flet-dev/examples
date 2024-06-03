8b3581af443c2ad5e260a752f961c19a651fc264
InesaFitsner <inesa@appveyor.com>
2024-06-03 12:33:49 -0700

    calc5 (to replace calc)

--- python/tutorials/calc/calc5.py
+++ python/tutorials/calc/calc5.py
@@ -4,8 +4,85 @@ import flet as ft
 class CalcButton(ft.ElevatedButton):
     def __init__(self, text, expand=1):
         super().__init__()
+        self.reset()
         self.text = text
         self.expand = expand
+        self.on_click = self.button_clicked
+
+    def button_clicked(self, e):
+        print(e.control)
+        data = e.control.text
+        self.result = self.page.calc.result
+        if self.page.calc.result.value == "Error" or data == "AC":
+            self.page.calc.result.value = "0"
+            self.reset()
+
+        elif data in ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."):
+            if self.page.calc.result.value == "0" or self.new_operand == True:
+                self.page.calc.result.value = data
+                self.new_operand = False
+            else:
+                self.page.calc.result.value = self.page.calc.result.value + data
+
+        elif data in ("+", "-", "*", "/"):
+            self.page.calc.result.value = self.calculate(
+                self.operand1, float(self.page.calc.result.value), self.operator
+            )
+            self.operator = data
+            if self.page.calc.result.value == "Error":
+                self.operand1 = "0"
+            else:
+                self.operand1 = float(self.page.calc.result.value)
+            self.new_operand = True
+
+        elif data in ("="):
+            self.page.calc.result.value = self.calculate(
+                self.operand1, float(self.page.calc.result.value), self.operator
+            )
+            self.reset()
+
+        elif data in ("%"):
+            self.page.calc.result.value = float(self.page.calc.result.value) / 100
+            self.reset()
+
+        elif data in ("+/-"):
+            if float(self.page.calc.result.value) > 0:
+                self.page.calc.result.value = "-" + str(self.page.calc.result.value)
+
+            elif float(self.page.calc.result.value) < 0:
+                self.page.calc.result.value = str(
+                    self.format_number(abs(float(self.page.calc.result.value)))
+                )
+
+        self.update()
+
+    def format_number(self, num):
+        if num % 1 == 0:
+            return int(num)
+        else:
+            return num
+
+    def calculate(self, operand1, operand2, operator):
+
+        if operator == "+":
+            return self.format_number(operand1 + operand2)
+
+        elif operator == "-":
+            return self.format_number(operand1 - operand2)
+
+        elif operator == "*":
+            return self.format_number(operand1 * operand2)
+
+        elif operator == "/":
+            if operand2 == 0:
+                return "Error"
+            else:
+                return self.format_number(operand1 / operand2)
+
+    def reset(self):
+        self.operator = "+"
+        self.operand1 = 0
+        self.new_operand = True
 
 
 class DigitButton(CalcButton):
@@ -88,10 +165,10 @@ class CalculatorApp(ft.Container):
 def main(page: ft.Page):
     page.title = "Calc App"
     # create application instance
-    calc = CalculatorApp()
+    page.calc = CalculatorApp()
 
     # add application's root control to the page
-    page.add(calc)
+    page.add(page.calc)
 
 
 ft.app(target=main)
