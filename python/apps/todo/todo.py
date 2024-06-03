6aacea3e546887fcf43d7708f37261cb1f71a0a0
InesaFitsner <inesa@appveyor.com>
2024-06-03 09:38:08 -0700

    changed main function

--- python/apps/todo/todo.py
+++ python/apps/todo/todo.py
@@ -164,13 +164,13 @@ class TodoApp(ft.UserControl):
         super().update()
 
 
-async def main(page: ft.Page):
+def main(page: ft.Page):
     page.title = "ToDo App"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.scroll = ft.ScrollMode.ADAPTIVE
 
     # create app control and add it to the page
-    await page.add_async(TodoApp())
+    page.add(TodoApp())
 
 
 ft.app(main)
