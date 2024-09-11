f32ae64b662375ce0e8f72bc27bc06476d3e6b68
InesaFitsner <inesa@appveyor.com>
2024-09-11 11:54:36 -0700

    Update 06_elevatedbutton_style.py

--- python/apps/controls-gallery/examples/buttons/elevatedbutton/06_elevatedbutton_style.py
+++ python/apps/controls-gallery/examples/buttons/elevatedbutton/06_elevatedbutton_style.py
@@ -1,29 +1,32 @@
 import flet as ft
 
-name = "ElevatedButton with style attributes configured for different MaterialState values"
+name = (
+    "ElevatedButton with style attributes configured for different ControlState values"
+)
+
 
 def example():
-    
+
     return ft.ElevatedButton(
-            "Styled button 1",
-            style=ft.ButtonStyle(
-                color={
-                    ft.MaterialState.HOVERED: ft.colors.WHITE,
-                    ft.MaterialState.FOCUSED: ft.colors.BLUE,
-                    ft.MaterialState.DEFAULT: ft.colors.BLACK,
-                },
-                bgcolor={ft.MaterialState.FOCUSED: ft.colors.PINK_200, "": ft.colors.YELLOW},
-                padding={ft.MaterialState.HOVERED: 20},
-                overlay_color=ft.colors.TRANSPARENT,
-                elevation={"pressed": 0, "": 1},
-                animation_duration=500,
-                side={
-                    ft.MaterialState.DEFAULT: ft.BorderSide(1, ft.colors.BLUE),
-                    ft.MaterialState.HOVERED: ft.BorderSide(2, ft.colors.BLUE),
-                },
-                shape={
-                    ft.MaterialState.HOVERED: ft.RoundedRectangleBorder(radius=20),
-                    ft.MaterialState.DEFAULT: ft.RoundedRectangleBorder(radius=2),
-                },
-            ),
-        )
+        "Styled button 1",
+        style=ft.ButtonStyle(
+            color={
+                ft.ControlState.HOVERED: ft.colors.WHITE,
+                ft.ControlState.FOCUSED: ft.colors.BLUE,
+                ft.ControlState.DEFAULT: ft.colors.BLACK,
+            },
+            bgcolor={ft.ControlState.FOCUSED: ft.colors.PINK_200, "": ft.colors.YELLOW},
+            padding={ft.ControlState.HOVERED: 20},
+            overlay_color=ft.colors.TRANSPARENT,
+            elevation={"pressed": 0, "": 1},
+            animation_duration=500,
+            side={
+                ft.ControlState.DEFAULT: ft.BorderSide(1, ft.colors.BLUE),
+                ft.ControlState.HOVERED: ft.BorderSide(2, ft.colors.BLUE),
+            },
+            shape={
+                ft.ControlState.HOVERED: ft.RoundedRectangleBorder(radius=20),
+                ft.ControlState.DEFAULT: ft.RoundedRectangleBorder(radius=2),
+            },
+        ),
+    )
