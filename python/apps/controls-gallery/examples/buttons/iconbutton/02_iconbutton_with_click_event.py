d67d85c2b92dd61ebc53ada743ff8ccbc5a371ef
InesaFitsner <inesa@appveyor.com>
2024-09-11 11:56:21 -0700

    Update 02_iconbutton_with_click_event.py

--- python/apps/controls-gallery/examples/buttons/iconbutton/02_iconbutton_with_click_event.py
+++ python/apps/controls-gallery/examples/buttons/iconbutton/02_iconbutton_with_click_event.py
@@ -4,10 +4,10 @@ name = "IconButton with `click` event"
 
 
 def example():
-    async def button_clicked(e):
+    def button_clicked(e):
         b.data += 1
         t.value = f"Button clicked {b.data} time(s)"
-        await t.update_async()
+        t.update()
 
     b = ft.IconButton(
         icon=ft.icons.PLAY_CIRCLE_FILL_OUTLINED, on_click=button_clicked, data=0
