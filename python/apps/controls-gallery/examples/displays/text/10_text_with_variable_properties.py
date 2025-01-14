5f271c593a60775e3de71a75bb04678b0ce11aca
InesaFitsner <inesa@appveyor.com>
2025-01-14 14:00:41 -0800

    Update 10_text_with_variable_properties.py

--- python/apps/controls-gallery/examples/displays/text/10_text_with_variable_properties.py
+++ python/apps/controls-gallery/examples/displays/text/10_text_with_variable_properties.py
@@ -12,28 +12,28 @@ def example():
         italic=True,
         selectable=True,
         text_align=ft.TextAlign.CENTER,
-        # badge=ft.Badge(text="Badge on the Text"),
-        # spans=[
-        #     ft.TextSpan(text="Span1", style=ft.TextStyle(size=30)),
-        #     ft.TextSpan(
-        #         text="Span2",
-        #         style=ft.TextStyle(size=30),
-        #     ),
-        # ],
+        badge=ft.Badge(text="Badge on the Text"),
+        spans=[
+            ft.TextSpan(text="Span1", style=ft.TextStyle(size=30)),
+            ft.TextSpan(
+                text="Span2",
+                style=ft.TextStyle(size=30),
+            ),
+        ],
         size=20,
         color=ft.Colors.GREEN_800,
         bgcolor=ft.Colors.GREEN_100,
         max_lines=2,
-        # tooltip=ft.Tooltip(),
-        # style=ft.TextStyle(
-        #     size=30,
-        #     shadow=[
-        #         ft.BoxShadow(spread_radius=5, blur_radius=10, color=ft.Colors.ORANGE)
-        #     ],
-        #     foreground=ft.Paint(
-        #         color=ft.Colors.BLUE_400, blend_mode=ft.BlendMode.COLOR_BURN
-        #     ),
-        # ),
+        tooltip=ft.Tooltip(""),
+        style=ft.TextStyle(
+            size=30,
+            shadow=[
+                ft.BoxShadow(spread_radius=5, blur_radius=10, color=ft.Colors.ORANGE)
+            ],
+            foreground=ft.Paint(
+                color=ft.Colors.BLUE_400, blend_mode=ft.BlendMode.COLOR_BURN
+            ),
+        ),
     )
 
     paint_properties_list = [
@@ -164,13 +164,13 @@ def example():
             "max": 100,
             "description": "An optional maximum number of lines for the text to span, wrapping if necessary. If the text exceeds the given number of lines, it will be truncated according to overflow.",
         },
-        # {
-        #     "name": "style",
-        #     "value_type": "dataclass",
-        #     "dataclass": ft.TextStyle,
-        #     "properties": style_properties_list,
-        #     "description": "The text's style.",
-        # },
+        {
+            "name": "style",
+            "value_type": "dataclass",
+            "dataclass": ft.TextStyle,
+            "properties": style_properties_list,
+            "description": "The text's style.",
+        },
         {
             "name": "badge",
             "value_type": "dataclass",
@@ -178,13 +178,13 @@ def example():
             "properties": badge_properties_list,
             "description": "Badges are used to show notifications, counts, or status information about its control.",
         },
-        # {
-        #     "name": "spans",
-        #     "value_type": "list",
-        #     "dataclass": ft.TextSpan,
-        #     "properties": span_properties_list,
-        #     "description": "The list of TextSpan objects to build a rich text paragraph.",
-        # },
+        {
+            "name": "spans",
+            "value_type": "list",
+            "dataclass": ft.TextSpan,
+            "properties": span_properties_list,
+            "description": "The list of TextSpan objects to build a rich text paragraph.",
+        },
     ]
 
     # properties = PropertiesTable(properties_list, t)
