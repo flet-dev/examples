bc5a0d82802d446c08d25e98184c81b18613115e
Owen McDonnell <omcdonnell@gmail.com>
2025-01-22 08:04:40 -0800

    alert-dialog

--- python/controls/alert-dialog/dialogs.py
+++ python/controls/alert-dialog/dialogs.py
@@ -4,13 +4,27 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "AlertDialog examples"
 
-    dlg = ft.AlertDialog(
-        title=ft.Text("Hello, you!"), on_dismiss=lambda e: print("Dialog dismissed!")
-    )
+    def open_dlg(e):
+        page.overlay.append(dlg)
+        dlg.open = True
+        page.update()
+
+    def open_dlg_modal(e):
+        page.overlay.append(dlg_modal)
+        dlg_modal.open = True
+        page.update()
 
     def close_dlg(e):
         dlg_modal.open = False
         page.update()
+
+    dlg = ft.AlertDialog(
+        title=ft.Text("Hello"),
+        content=ft.Text("You are notified!"),
+        alignment=ft.alignment.center,
+        on_dismiss=lambda e: print("Dialog dismissed!"),
+        title_padding=ft.padding.all(25),
+    )
 
     dlg_modal = ft.AlertDialog(
         modal=True,
@@ -24,20 +38,10 @@ def main(page: ft.Page):
         on_dismiss=lambda e: print("Modal dialog dismissed!"),
     )
 
-    def open_dlg(e):
-        page.dialog = dlg
-        dlg.open = True
-        page.update()
-
-    def open_dlg_modal(e):
-        page.dialog = dlg_modal
-        dlg_modal.open = True
-        page.update()
-
     page.add(
         ft.ElevatedButton("Open dialog", on_click=open_dlg),
         ft.ElevatedButton("Open modal dialog", on_click=open_dlg_modal),
     )
 
 
-ft.app(target=main)
+ft.app(main)
