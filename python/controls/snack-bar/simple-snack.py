029bff140552ade23e5dbba5827533895e74863f
Owen McDonnell <omcdonnell@gmail.com>
2025-01-21 22:14:36 -0800

    snackbar

--- python/controls/snack-bar/simple-snack.py
+++ python/controls/snack-bar/simple-snack.py
@@ -1,7 +1,6 @@
 import logging
+import flet as ft
 
-import flet
-from flet import ElevatedButton, SnackBar, Text
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -10,26 +9,30 @@ class Data:
     def __init__(self) -> None:
         self.counter = 0
 
+    def increment(self):
+        self.counter += 1
+
+    def decrement(self):
+        self.counter -= 1
+
 
 d = Data()
 
 
 def main(page):
-
-    page.snack_bar = SnackBar(
-        content=Text("Hello, world!"),
-        # remove_current_snackbar=True,
-        action="Alright!",
+    page.snack_bar = ft.SnackBar(
+        content=ft.Text(f"You did it!"),
+        action="Undo it!",
+        on_action=lambda e: d.decrement(),
     )
 
     def on_click(e):
-        # page.snack_bar.content.value = f"Hello, world: {d.counter}"
-        page.snack_bar = SnackBar(Text(f"Hello {d.counter}"))
+        d.increment()
+        page.snack_bar.content.value = f"You did it x {d.counter}"
         page.snack_bar.open = True
-        d.counter += 1
         page.update()
 
-    page.add(ElevatedButton("Open SnackBar", on_click=on_click))
+    page.add(ft.ElevatedButton("Open SnackBar", on_click=on_click))
 
 
-flet.app(target=main)
+ft.app(main)
