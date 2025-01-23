3948bf5fa727a5c24708400ba22e6557a890e4a7
Owen McDonnell <omcdonnell@gmail.com>
2025-01-23 13:43:02 -0800

    audio examples update

--- python/controls/audio/audio-player-async.py
+++ python/controls/audio/audio-player-async.py
@@ -7,37 +7,40 @@ url = "https://github.com/mdn/webaudio-examples/blob/main/audio-analyser/viper.m
 async def main(page: ft.Page):
     async def volume_down(_):
         audio1.volume -= 0.1
-        await audio1.update_async()
+        audio1.update()
 
     async def volume_up(_):
         audio1.volume += 0.1
-        await audio1.update_async()
+        audio1.update()
 
     async def balance_left(_):
         audio1.balance -= 0.1
-        await audio1.update_async()
+        audio1.update()
 
     async def balance_right(_):
         audio1.balance += 0.1
-        await audio1.update_async()
+        audio1.update()
 
     async def play(_):
-        await audio1.play_async()
+        audio1.play()
 
     async def pause(_):
-        await audio1.pause_async()
+        audio1.pause()
 
     async def resume(_):
-        await audio1.resume_async()
+        audio1.resume()
 
     async def release(_):
-        await audio1.release_async()
+        audio1.release()
 
     async def seek(_):
-        await audio1.seek_async(2000)
+        audio1.seek(3000)
 
     async def get_duration(_):
-        print("Current position:", await audio1.get_duration_async())
+        print("Current duration:", await audio1.get_duration_async())
+
+    async def get_position(_):
+        print("Current position:", await audio1.get_current_position_async())
 
     audio1 = ft.Audio(
         src=url,
@@ -51,12 +54,12 @@ async def main(page: ft.Page):
         on_seek_complete=lambda _: print("Seek complete"),
     )
     page.overlay.append(audio1)
-    await page.add_async(
+    page.add(
         ft.ElevatedButton("Play", on_click=play),
         ft.ElevatedButton("Pause", on_click=pause),
         ft.ElevatedButton("Resume", on_click=resume),
         ft.ElevatedButton("Release", on_click=release),
-        ft.ElevatedButton("Seek 2s", on_click=seek),
+        ft.ElevatedButton("Seek 3s", on_click=seek),
         ft.Row(
             [
                 ft.ElevatedButton("Volume down", on_click=volume_down),
@@ -72,7 +75,7 @@ async def main(page: ft.Page):
         ft.ElevatedButton("Get duration", on_click=get_duration),
         ft.ElevatedButton(
             "Get current position",
-            on_click=get_duration,
+            on_click=get_position,
         ),
     )
 
