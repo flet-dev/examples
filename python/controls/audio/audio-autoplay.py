c98a631ba03003cb1778f28f01928af8861e6bb3
Owen McDonnell <omcdonnell@gmail.com>
2025-01-23 13:43:02 -0800

    audio examples update

--- python/controls/audio/audio-autoplay.py
+++ python/controls/audio/audio-autoplay.py
@@ -1,16 +1,29 @@
-import flet
-from flet import Audio, ElevatedButton, Page, Text
+import flet as ft
 
 
-def main(page: Page):
-    audio1 = Audio(
-        src="https://luan.xyz/files/audio/ambient_c_motion.mp3", autoplay=True
+def main(page: ft.Page):
+    page.title = "Audio Example"
+
+    def change_button(e):
+        if e.state == ft.AudioState.PAUSED:
+            b.text = "Resume playing"
+            b.on_click = lambda e: audio1.resume()
+
+        elif e.state == ft.AudioState.PLAYING:
+            b.text = "Pause playing"
+            b.on_click = lambda e: audio1.pause()
+
+        b.update()
+
+    audio1 = ft.Audio(
+        src="https://luan.xyz/files/audio/ambient_c_motion.mp3",
+        autoplay=True,
+        on_state_changed=change_button,
     )
+    b = ft.ElevatedButton("Pause playing", on_click=lambda _: audio1.pause())
+
     page.overlay.append(audio1)
-    page.add(
-        Text("This is an app with background audio."),
-        ElevatedButton("Stop playing", on_click=lambda _: audio1.pause()),
-    )
+    page.add(ft.Text("This is an app with background audio."), b)
 
 
-flet.app(target=main)
+ft.app(main)
