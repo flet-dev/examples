2f590e47e8aded8e63441f4952c5c032ba8229c6
Owen McDonnell <omcdonnell@gmail.com>
2025-01-31 21:39:21 -0800

    add bottom-appbar example

---
+++ python/controls/appbar/bottom-appbar.py
@@ -0,0 +1,34 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.horizontal_alignment = page.vertical_alignment = "center"
+
+    page.floating_action_button = ft.FloatingActionButton(
+        icon=ft.Icons.ADD, shape=ft.CircleBorder()
+    )
+    page.floating_action_button_location = ft.FloatingActionButtonLocation.CENTER_DOCKED
+
+    page.appbar = ft.AppBar(
+        title=ft.Text("Bottom AppBar Demo"),
+        center_title=True,
+        bgcolor=ft.Colors.GREEN_300,
+        automatically_imply_leading=False,
+    )
+    page.bottom_appbar = ft.BottomAppBar(
+        bgcolor=ft.Colors.BLUE,
+        shape=ft.NotchShape.CIRCULAR,
+        content=ft.Row(
+            controls=[
+                ft.IconButton(icon=ft.Icons.MENU, icon_color=ft.Colors.WHITE),
+                ft.Container(expand=True),
+                ft.IconButton(icon=ft.Icons.SEARCH, icon_color=ft.Colors.WHITE),
+                ft.IconButton(icon=ft.Icons.FAVORITE, icon_color=ft.Colors.WHITE),
+            ]
+        ),
+    )
+
+    page.add(ft.Text("Body!"))
+
+
+ft.app(main)
