c567b43e8c7c1aff93b74b81f989d2b8d32cec3c
Owen McDonnell <omcdonnell@gmail.com>
2025-02-03 23:41:23 -0800

    add cupertino-segmented-button examples

---
+++ python/controls/cupertino-buttons/cupertino-segmented-button.py
@@ -0,0 +1,27 @@
+import flet as ft
+
+
+def main(page):
+    page.theme_mode = ft.ThemeMode.LIGHT
+
+    page.add(
+        ft.CupertinoSegmentedButton(
+            selected_index=1,
+            selected_color=ft.Colors.RED_400,
+            on_change=lambda e: print(f"selected_index: {e.data}"),
+            controls=[
+                ft.Text("One"),
+                ft.Container(
+                    padding=ft.padding.symmetric(0, 30),
+                    content=ft.Text("Two"),
+                ),
+                ft.Container(
+                    padding=ft.padding.symmetric(0, 10),
+                    content=ft.Text("Three"),
+                ),
+            ],
+        ),
+    )
+
+
+ft.app(main)
