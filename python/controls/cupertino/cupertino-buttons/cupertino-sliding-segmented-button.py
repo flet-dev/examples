17f072336f153dbf0aa4c6522ea32ce92b121da1
Owen McDonnell <omcdonnell@gmail.com>
2025-03-08 22:40:45 -0800

    update cupertino sliding segment button example

--- python/controls/cupertino/cupertino-buttons/cupertino-sliding-segmented-button.py
+++ python/controls/cupertino/cupertino-buttons/cupertino-sliding-segmented-button.py
@@ -7,15 +7,13 @@ def main(page):
 
     def handle_change(e):
         print(f"selected_index: {e.data}")
-        page.add(ft.Text(f"segment {int(e.data) + 1} chosen"))
-        page.update()
+        page.open(ft.SnackBar(ft.Text(f"segment {int(e.data) + 1} chosen")))
 
     page.add(
         ft.CupertinoSlidingSegmentedButton(
             selected_index=1,
             thumb_color=ft.Colors.BLUE_400,
             on_change=handle_change,
-            padding=ft.padding.symmetric(0, 10),
             controls=[
                 ft.Text("One"),
                 ft.Text("Two"),
