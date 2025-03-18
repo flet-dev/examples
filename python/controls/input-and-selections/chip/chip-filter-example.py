87391786d16ef976614173516ae6212d3b5cf497
Owen McDonnell <omcdonnell@gmail.com>
2025-03-17 23:24:57 -0700

    update chip examples

---
+++ python/controls/input-and-selections/chip/chip-filter-example.py
@@ -0,0 +1,26 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    def amenity_selected(e):
+        page.update()
+
+    title = ft.Row([ft.Icon(ft.Icons.HOTEL_CLASS), ft.Text("Amenities")])
+    amenities = ["Washer / Dryer", "Ramp access", "Dogs OK", "Cats OK", "Smoke-free"]
+    amenity_chips = []
+
+    for amenity in amenities:
+        amenity_chips.append(
+            ft.Chip(
+                label=ft.Text(amenity),
+                bgcolor=ft.Colors.GREEN_200,
+                disabled_color=ft.Colors.GREEN_100,
+                autofocus=True,
+                on_select=amenity_selected,
+            )
+        )
+
+    page.add(title, ft.Row(amenity_chips))
+
+
+ft.app(main)
