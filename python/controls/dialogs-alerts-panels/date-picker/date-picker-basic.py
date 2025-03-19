5f0b105e27f2f5cfccd659571a63c19f6ef8f69b
Owen McDonnell <omcdonnell@gmail.com>
2025-03-18 17:22:16 -0700

    add date-picker and time-picker examples

---
+++ python/controls/dialogs-alerts-panels/date-picker/date-picker-basic.py
@@ -0,0 +1,30 @@
+import datetime
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    def handle_change(e):
+        page.add(ft.Text(f"Date changed: {e.control.value.strftime('%m/%d/%Y')}"))
+
+    def handle_dismissal(e):
+        page.add(ft.Text(f"DatePicker dismissed"))
+
+    page.add(
+        ft.ElevatedButton(
+            "Pick date",
+            icon=ft.Icons.CALENDAR_MONTH,
+            on_click=lambda e: page.open(
+                ft.DatePicker(
+                    first_date=datetime.datetime(year=2000, month=10, day=1),
+                    last_date=datetime.datetime(year=2025, month=10, day=1),
+                    on_change=handle_change,
+                    on_dismiss=handle_dismissal,
+                )
+            ),
+        )
+    )
+
+
+ft.app(main)
---
+++ python/controls/dialogs-alerts-panels/time-picker/time-picker-basic.py
@@ -0,0 +1,34 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    def handle_change(e):
+        page.add(ft.Text(f"TimePicker change: {time_picker.value}"))
+
+    def handle_dismissal(e):
+        page.add(ft.Text(f"TimePicker dismissed: {time_picker.value}"))
+
+    def handle_entry_mode_change(e):
+        page.add(ft.Text(f"TimePicker Entry mode changed to {e.entry_mode}"))
+
+    time_picker = ft.TimePicker(
+        confirm_text="Confirm",
+        error_invalid_text="Time out of range",
+        help_text="Pick your time slot",
+        on_change=handle_change,
+        on_dismiss=handle_dismissal,
+        on_entry_mode_change=handle_entry_mode_change,
+    )
+
+    page.add(
+        ft.ElevatedButton(
+            "Pick time",
+            icon=ft.Icons.TIME_TO_LEAVE,
+            on_click=lambda _: page.open(time_picker),
+        )
+    )
+
+
+ft.app(main)
