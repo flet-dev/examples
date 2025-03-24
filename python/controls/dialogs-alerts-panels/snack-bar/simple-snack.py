d1ca95c1db14f0551d29fa054f2bdfd0732a4931
Owen McDonnell <omcdonnell@gmail.com>
2025-03-23 21:55:47 -0600

    add snack bar example

--- python/controls/dialogs-alerts-panels/snack-bar/simple-snack.py
+++ python/controls/dialogs-alerts-panels/snack-bar/simple-snack.py
@@ -1,40 +1,16 @@
-import logging
 import flet as ft
 
 
-logging.basicConfig(level=logging.DEBUG)
-
-
-class Data:
-    def __init__(self) -> None:
-        self.counter = 0
-
-    def increment(self):
-        self.counter += 1
-
-    def decrement(self):
-        self.counter -= 1
-
-
-d = Data()
-
-
-def main(page):
-    page.title = "SnackBar examples"
-
-    sb = ft.SnackBar(
-        content=ft.Text(f"You did it!"),
-        action="Undo it!",
-        on_action=lambda e: d.decrement(),
-    )
+def main(page: ft.Page):
+    counter = 0
 
     def on_click(e):
-        d.increment()
-        sb.content.value = f"You did it x {d.counter}"
-        page.open(sb)
+        nonlocal counter
+        page.open(ft.SnackBar(ft.Text(f"Counter value at {counter}")))
+        counter += 1
         page.update()
 
     page.add(ft.ElevatedButton("Open SnackBar", on_click=on_click))
 
 
-ft.app(main)
+ft.app(target=main)
