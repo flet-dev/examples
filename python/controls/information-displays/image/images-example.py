7159b1442113b8856b9a37c3fcc63fc5aaa51f42
Owen McDonnell <omcdonnell@gmail.com>
2025-03-29 23:55:01 -0700

    renames image example

---
+++ python/controls/information-displays/image/images-example.py
@@ -0,0 +1,34 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "Images Example"
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.padding = 50
+    page.update()
+
+    img = ft.Image(
+        src=f"/icons/icon-512.png",
+        width=100,
+        height=100,
+        fit=ft.ImageFit.CONTAIN,
+    )
+    images = ft.Row(expand=1, wrap=False, scroll=ft.ScrollMode.ALWAYS)
+
+    page.add(img, images)
+
+    for i in range(0, 30):
+        images.controls.append(
+            ft.Image(
+                src=f"https://picsum.photos/200/200?{i}",
+                width=200,
+                height=200,
+                fit=ft.ImageFit.NONE,
+                repeat=ft.ImageRepeat.NO_REPEAT,
+                border_radius=ft.border_radius.all(10),
+            )
+        )
+    page.update()
+
+
+ft.app(main)
