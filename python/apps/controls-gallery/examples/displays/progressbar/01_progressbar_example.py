cc713015af3c4a7d7309d23fc9684e9319327e44
InesaFitsner <inesa@appveyor.com>
2025-04-28 14:42:27 -0700

    Update 01_progressbar_example.py

--- python/apps/controls-gallery/examples/displays/progressbar/01_progressbar_example.py
+++ python/apps/controls-gallery/examples/displays/progressbar/01_progressbar_example.py
@@ -6,39 +6,90 @@ name = "ProgressBar Example"
 
 
 def example():
-    t = ft.Text(value="Click the button...")
-    pb = ft.ProgressBar(width=400, value=0)
+    class Example(ft.Column):
+        def __init__(self):
+            super().__init__()
+            self.t = ft.Text(value="Click the button...")
+            self.pb = ft.ProgressBar(width=400, value=0)
+            self.b = ft.FilledTonalButton("Start", on_click=self.button_clicked)
+            self.controls = [
+                ft.Text(
+                    "Linear progress indicator",
+                    theme_style=ft.TextThemeStyle.HEADLINE_SMALL,
+                ),
+                ft.Column([self.t, self.pb]),
+                ft.Text(
+                    "Indeterminate progress bar",
+                    theme_style=ft.TextThemeStyle.HEADLINE_SMALL,
+                ),
+                ft.ProgressBar(width=400, color="amber", bgcolor="#eeeeee"),
+                self.b,
+            ]
+            self.width = 400
+            self.height = 400
 
-    async def button_clicked(e):
-        t.value = "Doing something..."
-        t.update()
-        b.disabled = True
-        b.update()
-        for i in range(0, 101):
-            pb.value = i * 0.01
-            await asyncio.sleep(0.1)
-            pb.update()
-        t.value = "Click the button..."
-        # t.update()
-        b.disabled = False
-        # b.update()
+        async def button_clicked(self, e):
+            self.t.value = "Doing something..."
+            self.t.update()
+            self.b.disabled = True
+            self.b.update()
+            for i in range(0, 101):
+                self.pb.value = i * 0.01
+                await asyncio.sleep(0.1)
+                if self.pb_mounted:
+                    self.pb.update()
+                else:
+                    break
+            self.t.value = "Click the button..."
+            self.b.disabled = False
 
-    b = ft.FilledTonalButton("Start", on_click=button_clicked)
+        # happens when example is added to the page (when user chooses the ProgressBar control from the grid)
+        def did_mount(self):
+            print("Did mount")
+            self.pb_mounted = True
 
-    return ft.Column(
-        [
-            ft.Text(
-                "Linear progress indicator",
-                theme_style=ft.TextThemeStyle.HEADLINE_SMALL,
-            ),
-            ft.Column([t, pb]),
-            ft.Text(
-                "Indeterminate progress bar",
-                theme_style=ft.TextThemeStyle.HEADLINE_SMALL,
-            ),
-            ft.ProgressBar(width=400, color="amber", bgcolor="#eeeeee"),
-            b,
-        ],
-        width=400,
-        height=400,
-    )
+        # happens when example is removed from the page (when user chooses different control group on the navigation rail)
+        def will_unmount(self):
+            print("Will unmount")
+            self.pb_mounted = False
+
+    # t = ft.Text(value="Click the button...")
+    # pb = ft.ProgressBar(width=400, value=0)
+
+    # async def button_clicked(e):
+    #     t.value = "Doing something..."
+    #     t.update()
+    #     b.disabled = True
+    #     b.update()
+    #     for i in range(0, 101):
+    #         pb.value = i * 0.01
+    #         await asyncio.sleep(0.1)
+    #         pb.update()
+    #     t.value = "Click the button..."
+    #     # t.update()
+    #     b.disabled = False
+    #     # b.update()
+
+    # b = ft.FilledTonalButton("Start", on_click=button_clicked)
+
+    # return ft.Column(
+    #     [
+    #         ft.Text(
+    #             "Linear progress indicator",
+    #             theme_style=ft.TextThemeStyle.HEADLINE_SMALL,
+    #         ),
+    #         ft.Column([t, pb]),
+    #         ft.Text(
+    #             "Indeterminate progress bar",
+    #             theme_style=ft.TextThemeStyle.HEADLINE_SMALL,
+    #         ),
+    #         ft.ProgressBar(width=400, color="amber", bgcolor="#eeeeee"),
+    #         b,
+    #     ],
+    #     width=400,
+    #     height=400,
+    # )
+
+    pb_example = Example()
+
+    return pb_example
