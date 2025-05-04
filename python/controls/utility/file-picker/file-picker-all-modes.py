f5151095a88d88213199719966829bfbfb3e1fd3
Feodor Fitsner <feodor@appveyor.com>
2025-05-04 16:15:56 -0700

    FilePicker and uploads

--- python/controls/utility/file-picker/file-picker-all-modes.py
+++ python/controls/utility/file-picker/file-picker-all-modes.py
@@ -2,31 +2,58 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    def pick_files_result(e: ft.FilePickerResultEvent):
+    file_picker = ft.FilePicker()
+    page.services.append(file_picker)
+
+    # Pick files dialog
+    async def open_pick_files_dialog(e):
+        files = await file_picker.pick_files_async(allow_multiple=True)
         selected_files.value = (
-            ", ".join(map(lambda f: f.name, e.files)) if e.files else "Cancelled!"
+            ", ".join(map(lambda f: f.name, files)) if files else "Cancelled!"
         )
-        selected_files.update()
 
-    pick_files_dialog = ft.FilePicker(on_result=pick_files_result)
-    selected_files = ft.Text()
+    # Save file dialog
+    async def open_save_file_dialog(e):
+        save_file_path.value = await file_picker.save_file_async()
 
-    page.overlay.append(pick_files_dialog)
+    # Open directory dialog
+    async def open_get_directory_dialog(e):
+        directory_path.value = await file_picker.get_directory_path_async()
 
     page.add(
         ft.Row(
             [
-                ft.ElevatedButton(
+                ft.Button(
                     "Pick files",
                     icon=ft.Icons.UPLOAD_FILE,
-                    on_click=lambda _: pick_files_dialog.pick_files(
-                        allow_multiple=True
-                    ),
+                    on_click=open_pick_files_dialog,
                 ),
-                selected_files,
+                selected_files := ft.Text(),
             ]
-        )
+        ),
+        ft.Row(
+            [
+                ft.Button(
+                    "Save file",
+                    icon=ft.Icons.SAVE,
+                    on_click=open_save_file_dialog,
+                    disabled=page.web,
+                ),
+                save_file_path := ft.Text(),
+            ]
+        ),
+        ft.Row(
+            [
+                ft.Button(
+                    "Open directory",
+                    icon=ft.Icons.FOLDER_OPEN,
+                    on_click=open_get_directory_dialog,
+                    disabled=page.web,
+                ),
+                directory_path := ft.Text(),
+            ]
+        ),
     )
 
 
-ft.app(main)
+ft.run(main)
--- python/controls/utility/file-picker/file-picker-upload-progress.py
+++ python/controls/utility/file-picker/file-picker-upload-progress.py
@@ -1,57 +1,54 @@
-from typing import Dict
-
 import flet as ft
+
+state = {"picked_files": []}
 
 
 def main(page: ft.Page):
-    prog_bars: Dict[str, ft.ProgressRing] = {}
-    files = ft.Ref[ft.Column]()
-    upload_button = ft.Ref[ft.ElevatedButton]()
-
-    def file_picker_result(e: ft.FilePickerResultEvent):
-        upload_button.current.disabled = True if e.files is None else False
-        prog_bars.clear()
-        files.current.controls.clear()
-        if e.files is not None:
-            for f in e.files:
-                prog = ft.ProgressRing(value=0, bgcolor="#eeeeee", width=20, height=20)
-                prog_bars[f.name] = prog
-                files.current.controls.append(ft.Row([prog, ft.Text(f.name)]))
-        page.update()
+    prog_bars: dict[str, ft.ProgressRing] = {}
 
     def on_upload_progress(e: ft.FilePickerUploadEvent):
         prog_bars[e.file_name].value = e.progress
-        prog_bars[e.file_name].update()
 
-    file_picker = ft.FilePicker(
-        on_result=file_picker_result, on_upload=on_upload_progress
-    )
+    # add to services
+    file_picker = ft.FilePicker(on_upload=on_upload_progress)
+    page.services.append(file_picker)
 
-    def upload_files(e):
-        uf = []
-        if file_picker.result is not None and file_picker.result.files is not None:
-            for f in file_picker.result.files:
-                uf.append(
-                    ft.FilePickerUploadFile(
-                        f.name,
-                        upload_url=page.get_upload_url(f"dir/{f.name}", 60),
-                    )
+    async def pick_files(e):
+        # open pick files dialog
+        files = await file_picker.pick_files_async(allow_multiple=True)
+        print("Picked files:", files)
+        state["picked_files"] = files
+
+        # update progress bars
+        upload_button.disabled = len(files) == 0
+        prog_bars.clear()
+        upload_progress.controls.clear()
+        for f in files:
+            prog = ft.ProgressRing(value=0, bgcolor="#eeeeee", width=20, height=20)
+            prog_bars[f.name] = prog
+            upload_progress.controls.append(ft.Row([prog, ft.Text(f.name)]))
+
+    async def upload_files(e):
+        upload_button.disabled = True
+        file_picker.upload(
+            [
+                ft.FilePickerUploadFile(
+                    name=file.name,
+                    upload_url=page.get_upload_url(f"dir/{file.name}", 60),
                 )
-            file_picker.upload(uf)
-
-    # hide dialog in a overlay
-    page.overlay.append(file_picker)
+                for file in state["picked_files"]
+            ]
+        )
 
     page.add(
         ft.ElevatedButton(
             "Select files...",
             icon=ft.Icons.FOLDER_OPEN,
-            on_click=lambda _: file_picker.pick_files(allow_multiple=True),
+            on_click=pick_files,
         ),
-        ft.Column(ref=files),
-        ft.ElevatedButton(
+        upload_progress := ft.Column(),
+        upload_button := ft.ElevatedButton(
             "Upload",
-            ref=upload_button,
             icon=ft.Icons.UPLOAD,
             on_click=upload_files,
             disabled=True,
